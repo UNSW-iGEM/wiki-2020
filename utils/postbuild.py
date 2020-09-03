b1d0b7d52dffe2ec5089f1869cf1d83b6e6d4a86
Jack Robbers <jack@2pisoftware.com>
2020-09-03 14:20:39 +1000

    convert to 11ty

--- utils/postbuild.py
+++
@@ -1,7 +0,0 @@
-with open('dist/index.html', 'r') as file:
-    contents = file.read()
-
-contents = contents.replace('src="../assets/', 'src="assets/')
-
-with open('dist/index.html', 'w') as file:
-    file.write(contents)
