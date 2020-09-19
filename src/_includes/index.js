8362c294578c1ea1e07bf340451dd54268c337e2
Jack Robbers <jack@2pisoftware.com>
2020-09-19 13:00:21 +1000

    add webpack

---
+++ src/_includes/index.js
@@ -0,0 +1,8 @@
+// import 'bootstrap';
+console.log('test');
+// window.onload() = function() {
+//     http = new XMLHttpRequest;
+//     http.open("GET", "https://2020.igem.org/Team:UNSW_Australia/Team")
+//     http.send()
+//     console.log(http.responseText);
+// }
