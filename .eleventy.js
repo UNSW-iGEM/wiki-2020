0ee35cfd48f8bec839da24c68e5c048af993b662
Jack Robbers <jack@2pisoftware.com>
2020-10-11 17:49:36 +1100

    fix all css issues

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,18 @@ const yaml = require("js-yaml");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addDataExtension("yml", contents => yaml.safeLoad(contents));
 
-    // eleventyConfig.addPassthroughCopy("build");
+    eleventyConfig.addPassthroughCopy("build");
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addFilter("sortByName", function(values) {
         let vals = [...values];     // this *seems* to prevent collection mutation...
         return vals.sort((a, b) => a.data.name.localeCompare(b.data.name));
     });
+    eleventyConfig.addFilter("getFileType", function(values) {
+        return values.split('.').pop();
+    })
+    eleventyConfig.addFilter("imagePrefix", function(values) {
+        return "/assets/images/" + values;
+    })
     eleventyConfig.addFilter("cite", function(values) {
         // grab the paper name and stuff from the csv.
         return "(Li, Robbers 2030)";
